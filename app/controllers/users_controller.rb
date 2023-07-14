3f56c7b6f1b4a5be7701b051f13a04148d33a9a6
samuelconlon <conlonsp@yahoo.com>
2023-07-13 21:22:16 -0400

    create users_controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :user_unprocessable
+
+  def create
+    user = User.create!(user_params)
+    session[:user_id] = user.id
+    render json: user, include: ['user_organizations', 'user_organizations.donations'],status: :created
+  end
+
+  def show
+    render json: User.find(session[:user_id]), status: :ok
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :password, :password_confirmation, :avatar_url, :bio)
+  end
+
+  def user_unprocessable(e)
+    render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def user_not_found
+    render json: { errors: ['User not found'] }, status: :unauthorized
+  end
+end
