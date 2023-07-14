28e336aa12c7bafca5a636fccbec98fa56b29801
samuelconlon <conlonsp@yahoo.com>
2023-07-13 21:55:10 -0400

    create NavBar component

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function NavBar() {
+
+  return (
+    <div>
+      <h1>This is the NavBar</h1>
+      <NavLink
+        to='/'
+        exact='true'
+      >
+        Dashboard
+      </NavLink>
+      <NavLink
+        to='/about'
+      >
+        About
+      </NavLink>
+      <NavLink
+        to='/organizations'
+      >
+        Organizations
+      </NavLink>
+    </div>
+  )
+}
+
+export default NavBar
