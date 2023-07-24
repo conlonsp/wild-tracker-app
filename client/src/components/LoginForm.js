5e973569fe3c8a14b0150542033794d2627fc6e4
samuelconlon <conlonsp@yahoo.com>
2023-07-24 12:32:28 -0400

    update LoginForm styling

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -46,39 +46,57 @@ function LoginForm({ setToLogin }) {
       }
     })
   }
-
+  
   return (
-    <div>
-      <h1>LoginForm</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type='text'
-          name='username'
-          placeholder="Username"
-          value={logUser.username}
-          onChange={handleChange}
-        />
-        <input
-          type='password'
-          name='password'
-          placeholder="Password"
-          value={logUser.password}
-          onChange={handleChange}
-        />
-        <button type='submit'>Login</button>
-      </form>
-      {errors.map(err => {
-        return (
-          <p key={err} style={{color: 'red'}}>{err}</p>
-        )
-      })}
-      <span>
-        <p>
-          Don't have an account?
-          &nbsp;
-          <button onClick={()=>setToLogin(true)}>Signup</button>
-        </p>
-      </span>
+    <div class="box-root flex-flex flex-direction--column container" style={{minHeight: '100vh', flexGrow: '1'}}>
+      <div class="formbg-outer">
+        <div class="formbg">
+          <div class="formbg-inner padding-horizontal--48">
+            <span class="padding-bottom--15">Sign in to your account</span>
+            <form onSubmit={handleSubmit}>
+              <div class="field padding-bottom--24">
+                <label>Username</label>
+                <input
+                  type='text'
+                  name='username'
+                  value={logUser.username}
+                  onChange={handleChange}
+                />
+              </div>
+              <div class="field padding-bottom--24">
+                <div class="padding-top--15">
+                  <label>Password</label>
+                </div>
+                <input
+                  type='password'
+                  name='password'
+                  value={logUser.password}
+                  onChange={handleChange}
+                />
+              </div>
+              <div class="field padding-bottom--24 padding-top--24">
+                <input type="submit" name="submit" value="Continue"/>
+              </div>
+              {errors.map(err => {
+                return (
+                  <p key={err} style={{color: 'red'}}>{err}</p>
+                )
+              })}
+              <div class="footer-link padding-top--24">
+                <span>
+                  Don't have an account?
+                  <p
+                    onClick={() => setToLogin(true)}
+                    style={{color: "rgb(84, 105, 212)", cursor: "pointer"}}
+                  >
+                    Sign up
+                  </p>
+                </span>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
@@ -94,3 +112,37 @@ export default LoginForm
 // :
 //   null
 // }
+
+            {/* <form id="stripe-login">
+              <div class="field padding-bottom--24">
+                <label for="email">Email</label>
+                <input type="email" name="email"/>
+              </div>
+              <div class="field padding-bottom--24">
+                <div class="grid--50-50">
+                  <label for="password">Password</label>
+                </div>
+                <input type="password" name="password"/>
+              </div>
+              <div class="field padding-bottom--24">
+                <input type="submit" name="submit" value="Continue"/>
+              </div>
+            </form> */}
+
+
+    // <div>
+    //   <h1>LoginForm</h1>
+
+    //   {errors.map(err => {
+    //     return (
+    //       <p key={err} style={{color: 'red'}}>{err}</p>
+    //     )
+    //   })}
+    //   <span>
+    //     <p>
+    //       Don't have an account?
+    //       &nbsp;
+    //       <button onClick={()=>setToLogin(true)}>Signup</button>
+    //     </p>
+    //   </span>
+    // </div>
