8bfae95e65831ff802decafecc29fb23187bf11c
samuelconlon <conlonsp@yahoo.com>
2023-07-26 11:35:51 -0400

    add style to DonationForm and UpdateOrg

--- client/src/components/DonationForm.js
+++ client/src/components/DonationForm.js
@@ -39,27 +39,69 @@ function DonationForm({ orgId, donations, setDonations }) {
 
   return (
     <div>
-      <h1>Donation Form</h1>
-      <p>{orgId}</p>
-      <form onSubmit={handleSubmit}>
-        <input
-          type='number'
-          name='amount'
-          placeholder='Amount'
-          value={donation.amount}
-          onChange={handleChange}
-        />
-        <input
-          type='text'
-          name='note'
-          placeholder='Note'
-          value={donation.note}
-          onChange={handleChange}
-        />
-        <button type='submit'>Submit</button>
-      </form>
+      <div class="formbg-outer" style={{maxWidth: '1000px'}}>
+        <div class="formbg">
+          <div class="formbg-inner padding-horizontal--48">
+            <span class="padding-bottom--15">Donate</span>
+            <form onSubmit={handleSubmit}>
+              <div class="field">
+                <div>
+                  <label>Amount</label>
+                </div>
+                <input
+                  type='text'
+                  name='amount'
+                  value={donation.amount}
+                  onChange={handleChange}
+                />
+              </div>
+              <div class="field">
+                <div class="padding-top--10">
+                  <label>Note</label>
+                </div>
+                <input
+                  type='text'
+                  name='note'
+                  value={donation.note}
+                  onChange={handleChange}
+                />
+              </div>
+              <div class="field padding-bottom--24 padding-top--24">
+                <input type="submit" name="submit" value="Donate"/>
+              </div>
+              {/* {errors.map(err => {
+                return (
+                  <p key={err} class='error'>{err}</p>
+                )
+              })} */}
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
 export default DonationForm
+
+    // <div>
+    //   <h1>Donation Form</h1>
+    //   <p>{orgId}</p>
+    //   <form onSubmit={handleSubmit}>
+    //     <input
+    //       type='number'
+    //       name='amount'
+    //       placeholder='Amount'
+    //       value={donation.amount}
+    //       onChange={handleChange}
+    //     />
+    //     <input
+    //       type='text'
+    //       name='note'
+    //       placeholder='Note'
+    //       value={donation.note}
+    //       onChange={handleChange}
+    //     />
+    //     <button type='submit'>Submit</button>
+    //   </form>
+    // </div>
--- client/src/pages/UpdateOrg.js
+++ client/src/pages/UpdateOrg.js
@@ -47,42 +47,100 @@ function UpdateOrg({ organization, onUpdate }) {
   }
   
   return (
-    <div className='container'>
+    <div class="box-root flex-flex flex-direction--column container" style={{minHeight: '100vh', flexGrow: '1'}}>
       {!isSuccessful ?
-        <div>
-          <h1>Update an Organization!</h1>
-          <form onSubmit={handleSubmit}>
-            <input
-              type='text'
-              name='name'
-              value={updated.name}
-              onChange={handleChange}
-            />
-            <input
-              type='text'
-              name='location'
-              value={updated.location}
-              onChange={handleChange}
-            />
-            <textarea
-              type='text'
-              name='mission'
-              value={updated.mission}
-              onChange={handleChange}
-            />
-            <button type='submit'>Submit</button>
-          </form>
-          {errors.map(err => {
-            return (
-              <p key={err} style={{color: 'red'}}>{err}</p>
-            )
-          })}
+      <div class="formbg-outer">
+        <div class="formbg">
+          <div class="formbg-inner padding-horizontal--48">
+            <span class="padding-bottom--15">Create an Organization</span>
+            <form onSubmit={handleSubmit}>
+              <div class="field">
+                <label>Organization Name</label>
+                <input
+                  type='text'
+                  name='name'
+                  value={updated.name}
+                  onChange={handleChange}
+                />
+              </div>
+              <div class="field">
+                <div class="padding-top--10">
+                  <label>Location</label>
+                </div>
+                <input
+                  type='text'
+                  name='location'
+                  value={updated.location}
+                  onChange={handleChange}
+                />
+              </div>
+              <div class="field">
+                <div class="padding-top--10">
+                  <label>Mission</label>
+                </div>
+                <textarea
+                  type='text'
+                  name='mission'
+                  value={updated.mission}
+                  onChange={handleChange}
+                />
+              </div>
+              <div class="field padding-bottom--24 padding-top--24 grid--25-75">
+                <input type='button' name='back' value="<"  onClick={() => navigate(`/organizations/${params.id}`)}/>
+                <input type="submit" name="submit" value="Continue"/>
+              </div>
+              {errors.map(err => {
+                return (
+                  <p key={err} class='error'>{err}</p>
+                )
+              })}
+            </form>
+          </div>
         </div>
+      </div>
       :
-        <h1>Submit Successful!</h1>
+      <div class="field text-blurb">
+        <h1 >Update Complete</h1>
+        <input type='button' name='back' value={`Back to ${organization.name}`} onClick={() => navigate(`/organizations/${params.id}`)} style={{opacity: 'none'}}/>
+      </div>
       }
-      <button onClick={() => navigate(`/organizations/${params.id}`)}>Close</button>
     </div>
+    // <div className='container'>
+    //   {!isSuccessful ?
+    //     <div>
+    //       <h1>Update an Organization!</h1>
+    //       <form onSubmit={handleSubmit}>
+    //         <input
+    //           type='text'
+    //           name='name'
+    //           value={updated.name}
+    //           onChange={handleChange}
+    //         />
+    //         <input
+    //           type='text'
+    //           name='location'
+    //           value={updated.location}
+    //           onChange={handleChange}
+    //         />
+    //         <textarea
+    //           type='text'
+    //           name='mission'
+    //           value={updated.mission}
+    //           onChange={handleChange}
+    //         />
+    //         <button type='submit'>Submit</button>
+    //       </form>
+    //       {errors.map(err => {
+    //         return (
+    //           <p key={err} style={{color: 'red'}}>{err}</p>
+    //         )
+    //       })}
+    //     </div>
+    //   :
+    //     <h1>Submit Successful!</h1>
+    //   }
+    //   <button onClick={() => navigate(`/organizations/${params.id}`)}>Close</button>
+    // </div>
   )
 }
 
